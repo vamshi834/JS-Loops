e76ae51eb821b091ccea6d9c1a4af9bf70d4f9d9
Vamshi Medipally <vamshi.m834@gmail.com>
2021-10-26 08:37:27 +0530

    added a file

---
+++ index.js
@@ -0,0 +1,26 @@
+console.clear()
+//while loop
+//while(condition){
+// block of code
+//}
+var num = 0
+while (num <= 50){
+  num++
+}
+console.log(num)
+
+
+var num = '1'
+while (num <= 20){
+  num++
+}
+console.log(num)
+
+//for loop
+
+var array1 = ['Duster', 'Indigo', 'City', 'Civic', 'Amaze', 'Nano']
+var pos = 0
+for(pos; pos < array1.length;pos++){
+  console.log("Position of car is " + pos +  "name of the car is " +array1[pos])
+}
+
